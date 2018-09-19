d2f9594ffb62546d919157567e7ac27d5d5d32cc
chenbo <chenbotome@163.com>
2018-09-19 16:28:20 +0800

    fix自定义命令（test） php think test {id}

--- application/index/command/TestCommand.php
+++ application/index/command/TestCommand.php
@@ -18,16 +18,25 @@ class TestCommand extends BaseCommand
 
     protected function configure()
     {
-        $this->setName('test')->setDescription('测试脚本处理命令');
+        $this->setName('test')
+            ->addArgument('id')
+            ->setDescription('测试脚本处理命令');
     }
 
     protected function execute(Input $input, Output $output)
     {
 //        脚本处理逻辑
-        $this->getPostService()->updateById(['title' => date('H:i:s', time()) . 'command处理'], 61);
-        $result = $this->getPostService()->getById(61);
-        $output->info("脚本处理结果说明信息");
-        $output->info(var_export($result));
+        $id = $input->getArgument('id');
+        if ($this->getPostService()->checkExistsById($id)) {
+            $this->getPostService()->updateById(['title' => date('H:i:s', time()) . 'command处理'], 61);
+            $result = $this->getPostService()->getById($id);
+            $output->info("记录{$id}的脚本处理结果说明信息");
+            $output->info(var_export($result));
+        } else {
+            $output->info("记录{$id}不存在");
+            $result = $this->getPostService()->listWithPaginate(1, 1);
+            $output->info(var_export($result));
+        }
     }
 
     private function getPostService(){
