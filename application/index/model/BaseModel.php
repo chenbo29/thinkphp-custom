e92a5c61bbad58cbbe7f6914b7b998796fdd5190
chenbo <chenbotome@163.com>
2018-09-20 11:03:38 +0800

    注释

--- application/index/model/BaseModel.php
+++ application/index/model/BaseModel.php
@@ -22,16 +22,38 @@ class BaseModel extends Model
     protected $table;
     protected $perPage;
 
+    /**
+     * 通过id获取表数据记录
+     * @param $id 记录id
+     * @return array|false|\PDOStatement|string|Model
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
     public function getById($id){
         $result = Db::table($this->table)->where('id', $id)->find();
         return $result;
     }
 
+    /**
+     * 分页数据
+     * @param $page 页码
+     * @param null $perPage 每页显示的数据记录
+     * @return false|\PDOStatement|string|\think\Collection
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
     public function listWithPaginate($page, $perPage = null){
         $result = Db::table($this->table)->page($page, $perPage)->select();
         return $result;
     }
 
+    /**
+     * 插入数据
+     * @param $fields 数据字段
+     * @return int|string
+     */
     public function insertWithFields($fields){
         $createAt = date('Y-m-d H:i:s', time());
         $updateAt = date('Y-m-d H:i:s', time());
@@ -39,7 +61,15 @@ class BaseModel extends Model
         return Db::table($this->table)->insertGetId($fields);
     }
 
+    /**
+     * @param $fields
+     * @param $where 条件
+     * @return int|string
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
     public function updateByWhere($fields, $where){
+        // fixme
         $updateAt = date('Y-m-d H:i:s', time());
         $fields = array_merge($fields, ['updated_at' => $updateAt]);
         $table = Db::table($this->table);
@@ -49,10 +79,23 @@ class BaseModel extends Model
         return $table->update($fields);
     }
 
+    /**
+     * 删除数据
+     * @param $id
+     * @return int
+     */
     public function deleteById($id){
         return $this->where('id', '=', $id)->delete();
     }
 
+    /**
+     * 通过id判断记录数据是否存在
+     * @param $id
+     * @return bool
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
     public function checkExistsById($id){
         return Db::table($this->table)->where('id', $id)->find() ? true : false;
     }
