46363ee1864f0a3ff583df69cfe9e9caab7493cf
chenbo <chenbotome@163.com>
2018-09-20 10:52:25 +0800

    注释

--- application/index/model/Impl/PostImplModel.php
+++ application/index/model/Impl/PostImplModel.php
@@ -13,11 +13,22 @@ namespace app\index\model\impl;
 use app\index\model\BaseModel;
 use app\index\model\PostModel;
 
+/**
+ * 文章模型类
+ * Class PostImplModel
+ * @package app\index\model\impl
+ */
 class PostImplModel extends BaseModel implements PostModel
 {
     protected $table = 'posts';
     public $perPage = 5;
 
+    /**
+     * 通过id更新post数据
+     * @param array $fields
+     * @param $id
+     * @return int|string
+     */
     public function updateById(array $fields, $id)
     {
         return $this->updateByWhere($fields, array("id" => $id));
