f16f1a84323e9dc5833d53ecbc9a8e077fe3926f
chenbo <chenbotome@163.com>
2018-09-29 10:02:45 +0800

    fix优化接口的自动化测试

--- tests/api/Base.php
+++ tests/api/Base.php
@@ -6,16 +6,55 @@
  * Time: 15:23
  */
 
+use Codeception\Module\Db;
+use Codeception\Module\Redis;
 class Base
 {
-    protected $accessKey;
-    protected $time;
+    protected $username = 'test';
+    protected $password = '123456';
+    protected $accessKey = "testAccessKey";
+    protected $secretKey = "testSecretKey";
 
+    /**
+     * api接口请求默认统一的参数
+     * @param array $params
+     * @return string
+     */
     protected function generateParamUrl($params = []){
-        return http_build_query(['ak' => $this->accessKey, 'time' => $this->time]);
+        $baseParams = ['ak' => $this->accessKey, 'time' => time()];
+        $httpParams = array_merge($params, $baseParams);
+        return http_build_query($httpParams);
     }
 
+    /**
+     * 获取接口返回的结果内容
+     * @param ApiTester $api
+     * @return array
+     */
     protected function getResult(ApiTester $api){
         return json_decode($api->grabResponse(), true);
+    }
+
+    /**
+     * 初始化一个用户
+     * @param Db $db
+     * @return array
+     */
+    protected function initUser(Db $db){
+        $user = [
+            'username' => $this->username,
+            'password' => password_hash($this->password, PASSWORD_BCRYPT)
+        ];
+        $db->haveInDatabase('user', $user);
+        return [$this->username, $this->password];
+    }
+
+    /**
+     * 初始化用户的会话信息
+     * @param Redis $redis
+     * @throws \Codeception\Exception\ModuleException
+     */
+    protected function initUserSession(Redis $redis){
+        $redis->haveInRedis('string',"auth:{$this->accessKey}", json_encode(['username' => $this->username, 'secretKey' => $this->secretKey]));
     }
 }
--- tests/api/PostCest.php
+++ tests/api/PostCest.php
@@ -4,56 +4,45 @@ class PostCest extends Base
 {
     protected $postId;
     protected $post;
-    public function _before(ApiTester $api, \Codeception\Module\Db $db)
-    {
-        $api->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
-        $api->sendPOST('/login', ['username' => 'chenbo', 'password' => '123456']);
-        $api->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
-        $result = json_decode($api->grabResponse(), true);
-        $this->accessKey = $result['data']['accessKey'];
-        $this->time = time();
-    }
 
-    public function testList(ApiTester $api, \Codeception\Module\Db $db){
-        list($postId, $post) = $this->insertDemoData($db);
+    public function _before(ApiTester $api, \Codeception\Module\Db $db, \Codeception\Module\Redis $redis)
+    {
+        $this->initUser($db);
+        $this->initUserSession($redis);
+        list($this->postId, $this->post) = $this->insertDemoData($db);
         $api->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
         $api->haveHttpHeader('Auth-Token', 'test');
+    }
+
+    public function testList(ApiTester $api){
         $api->sendGET("/post?{$this->generateParamUrl()}");
         $api->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
         $api->seeResponseIsJson();
-        $api->seeResponseContainsJson(['code' => \app\index\ResponseCode::statusSuccess, 'msg' => '', 'data' => [$post] ]);
+        $api->seeResponseContainsJson(['code' => \app\index\ResponseCode::statusSuccess, 'msg' => '', 'data' => [$this->post] ]);
     }
 
-    public function testUpdate(ApiTester $api, \Codeception\Module\Db $db){
-        list($postId, $post) = $this->insertDemoData($db);
+    public function testUpdate(ApiTester $api){
         $title = date('Y-m-d H:i:s') . '标题';
         $content = date('Y-m-d H:i:s') . '内容';
         $newPost = ['title' => $title, 'content' => $content];
-        $api->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
-        $api->haveHttpHeader('Auth-Token', 'test');
-        $api->sendPUT("/post/{$postId}?{$this->generateParamUrl()}", $newPost);
+        $api->sendPUT("/post/{$this->postId}?{$this->generateParamUrl()}", $newPost);
         $api->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
         $api->seeResponseIsJson();
         $api->seeResponseContainsJson(['code' => \app\index\ResponseCode::statusSuccess, 'msg' => '编辑保存成功', 'data' => []]);
     }
 
-    public function testRead(ApiTester $api, \Codeception\Module\Db $db)
+    public function testRead(ApiTester $api)
     {
-        list($postId, $post) = $this->insertDemoData($db);
-        $api->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
-        $api->haveHttpHeader('Auth-Token', 'test');
-        $api->sendGET("/post/{$postId}?{$this->generateParamUrl()}");
+        $api->sendGET("/post/{$this->postId}?{$this->generateParamUrl()}");
         $api->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
         $api->seeResponseIsJson();
-        $api->seeResponseContainsJson(['code' => \app\index\ResponseCode::statusSuccess, 'msg' => '', 'data' => $post]);
+        $api->seeResponseContainsJson(['code' => \app\index\ResponseCode::statusSuccess, 'msg' => '', 'data' => $this->post]);
     }
 
-    public function testSave(ApiTester $api, \Codeception\Module\Db $db)
+    public function testSave(ApiTester $api)
     {
         $title = date('Y-m-d H:i:s') . '标题';
         $content = date('Y-m-d H:i:s') . '内容';
-        $api->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
-        $api->haveHttpHeader('Auth-Token', 'test');
         $api->sendPOST("/post?{$this->generateParamUrl()}", ['title' => $title, 'content' => $content]);
         $api->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
         $api->seeResponseIsJson();
@@ -61,8 +50,16 @@ class PostCest extends Base
         $api->seeResponseContainsJson(['code' => \app\index\ResponseCode::statusSuccess, 'msg' => '保存成功', 'data' => ['id' => $response['data']['id']]]);
     }
 
+    public function testDelete(ApiTester $api)
+    {
+        $api->sendDELETE("/post/{$this->postId}?{$this->generateParamUrl()}");
+        $api->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
+        $api->seeResponseIsJson();
+        $api->seeResponseContainsJson(['code' => \app\index\ResponseCode::statusSuccess, 'msg' => '删除成功']);
+    }
+
     private function insertDemoData($db){
-        $post = ['title' => 'test title', 'content' => 'test content', 'create_time' => time(), 'update_time'=>time()];
+        $post = ['id' => 1, 'title' => 'test title', 'content' => 'test content', 'create_time' => time(), 'update_time'=>time()];
         $postId = $db->haveInDatabase('post', $post);
         return [$postId, $post];
     }
